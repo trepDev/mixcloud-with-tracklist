999a1754d8b918647778357e666e5f424c60ead7
trepDev <trexpu@gmail.com>
2019-05-07 14:23:08 +0200

    -add copyright -add use strict

--- src/utils/domUtil.js
+++ src/utils/domUtil.js
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict'
 function htmlToNode (html) {
   let temp = document.createElement('div')
   temp.insertAdjacentHTML('afterbegin', html)
