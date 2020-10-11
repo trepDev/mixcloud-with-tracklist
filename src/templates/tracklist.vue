55438c6410d4f1d367903015f50aeb0dd3dc0f5d
c82atrz <Ez3_ki10>
2020-10-11 17:54:56 +0200

    add Copy to clipboard functionality

--- src/templates/tracklist.vue
+++ src/templates/tracklist.vue
@@ -7,6 +7,7 @@
                 <div class="header-timestamp">Time</div>
                 <div class="header-track">Song</div>
                 <div class="header-artist">Artist</div>
+                <a class="header-copy btn btn-inverse" v-on:click="copyToClipoard(tracklist)"><svg height="10pt" viewBox="-40 0 512 512" width="10pt" xmlns="http://www.w3.org/2000/svg"><path d="m271 512h-191c-44.113281 0-80-35.886719-80-80v-271c0-44.113281 35.886719-80 80-80h191c44.113281 0 80 35.886719 80 80v271c0 44.113281-35.886719 80-80 80zm-191-391c-22.054688 0-40 17.945312-40 40v271c0 22.054688 17.945312 40 40 40h191c22.054688 0 40-17.945312 40-40v-271c0-22.054688-17.945312-40-40-40zm351 261v-302c0-44.113281-35.886719-80-80-80h-222c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h222c22.054688 0 40 17.945312 40 40v302c0 11.046875 8.953125 20 20 20s20-8.953125 20-20zm0 0"/></svg></a>
             </div>
             <div v-for="track in tracklist" class="table-row" >
                 <div id='trackNumber' class="row-number"
@@ -52,7 +53,12 @@
       getTitleAttribute: function(timestamp) {
         return hasTimestamp(timestamp) ? 'play' : 'no play'
       },
-      hasTimestamp: hasTimestamp
+      hasTimestamp: hasTimestamp,
+      copyToClipoard: function (tracklist) {
+        let toCopy = '';
+        tracklist.forEach((track) => toCopy = toCopy + track.trackNumber + ' - ' + track.songName + ' - ' + track.artistName + '\n');
+        navigator.clipboard.writeText(toCopy);
+      }
     }
   }
 
@@ -96,10 +102,17 @@
         width: 15%;
     }
     .header-track{
-        width: 40%;
+        width: 37.5%;
     }
     .header-artist{
-        width: 40%;
+        width: 37.5%;
+    }
+
+    .header-copy{
+        width: 5%;
+        padding-left: 12px;
+      padding-bottom: 0px;
+      padding-top: 7px;
     }
 
     .row-number{
