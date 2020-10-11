ec2de7076f1be5f09f502913664ce296f6185160
trepDev <trepdev@fastmail.com>
2020-10-11 17:36:01 +0200

    Fix build Dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,6 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  devtool: 'cheap-module-source-map'
 })
