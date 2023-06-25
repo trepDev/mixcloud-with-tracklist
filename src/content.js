229ef21b6e070df4575f7f817d4a1eba21b7ccc8
trepDev <trepdev@fastmail.com>
2023-06-25 21:52:54 +0200

    remove check on user logged (work also if user is not logged)

--- src/content.js
+++ src/content.js
@@ -14,22 +14,18 @@ let currentPath = document.location.pathname
 let tracklistActivated = false
 
 const mutationObserver = new MutationObserver(function (mutations) {
-  // If user is logged
-  if (document.querySelector('[class^="UserDropdown__ProfileIconWrapper"]') ||
-    document.querySelector('[class^="Header__Notification"]')) {
-    // Condition to avoid activate tracklist at each node mutation
-    if (document.location.pathname !== currentPath) {
-      currentPath = document.location.pathname
-      tracklistActivated = false
-    }
-    const bodyDetailNode = document.querySelector('[class^="Layouts__RightSidebarLayout"]')
-    const cloudcastSideBar = document.querySelector('[class^="CloudcastBaseSidebar"]')
-    const loadingCloudcastSideBar = document.querySelector('[class^="CloudcastBaseSidebar"] > [class^="Loader__Wrapper"]')
+  // Condition to avoid activate tracklist at each node mutation
+  if (document.location.pathname !== currentPath) {
+    currentPath = document.location.pathname
+    tracklistActivated = false
+  }
+  const bodyDetailNode = document.querySelector('[class^="Layouts__RightSidebarLayout"]')
+  const cloudcastSideBar = document.querySelector('[class^="CloudcastBaseSidebar"]')
+  const loadingCloudcastSideBar = document.querySelector('[class^="CloudcastBaseSidebar"] > [class^="Loader__Wrapper"]')
 
-    if (bodyDetailNode && cloudcastSideBar && !loadingCloudcastSideBar && !tracklistActivated && !document.getElementById('toogleTracklist')) {
-      tracklistActivated = true
-      activateTracklist(currentPath)
-    }
+  if (bodyDetailNode && cloudcastSideBar && !loadingCloudcastSideBar && !tracklistActivated && !document.getElementById('toogleTracklist')) {
+    tracklistActivated = true
+    activateTracklist(currentPath)
   }
 })
 
