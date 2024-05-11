22d99c087b4af92d347f50b2db1d5a037cce9ee3
trepDev <trepdev@fastmail.com>
2024-05-11 18:23:39 +0200

    conf to delete console.log in build prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,22 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = (env) => {
   return merge(common(env), {
     mode: 'production',
     optimization: {
-      minimize: true
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              // Specify functions to drop
+              pure_funcs: ['console.log']
+            }
+          }
+        })
+      ]
     }
   })
 }
