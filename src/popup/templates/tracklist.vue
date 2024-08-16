4de97a99fd400a25ecea1bf65760e9a800243164
trepDev <trepdev@fastmail.com>
2024-08-16 17:09:10 +0200

    tracklist is now a true html table

--- src/popup/templates/tracklist.vue
+++ src/popup/templates/tracklist.vue
@@ -2,38 +2,48 @@
   <div v-if="tracklist.length === 0" style="min-width: 780px; padding: 24px; text-align: center;">
     <p style="font-size: 18px; font-weight: 400;">Sorry but the DJ didn't provide any tracklist for this mix.</p>
   </div>
-    <div v-else class="mwt-tracklist-container">
-      <div class="mwt-header">
-        <div class="header-number">#</div>
-        <div class="header-timestamp">Time</div>
-        <div class="header-artist">Artist</div>
-        <div class="header-track">Song</div>
-        <a class="header-copy copy-button" v-on:click="copyToClipoard(tracklist)"><svg height="10pt" viewBox="-40 0 512 512" width="10pt" xmlns="http://www.w3.org/2000/svg"><path d="m271 512h-191c-44.113281 0-80-35.886719-80-80v-271c0-44.113281 35.886719-80 80-80h191c44.113281 0 80 35.886719 80 80v271c0 44.113281-35.886719 80-80 80zm-191-391c-22.054688 0-40 17.945312-40 40v271c0 22.054688 17.945312 40 40 40h191c22.054688 0 40-17.945312 40-40v-271c0-22.054688-17.945312-40-40-40zm351 261v-302c0-44.113281-35.886719-80-80-80h-222c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h222c22.054688 0 40 17.945312 40 40v302c0 11.046875 8.953125 20 20 20s20-8.953125 20-20zm0 0"/></svg></a>
-      </div>
-      <div v-for="track in tracklist" class="table-row" >
-        <div id='trackNumber' class="row-number"
-             v-bind:class="{ 'pointer': hasTimestamp(track.timestamp) }"
-             v-bind:title="getTitleAttribute(track.timestamp)"
-             v-on:click="callContentToPlayTrack(track.timestamp)">
-          {{track.trackNumber}}
-        </div>
-        <div id='timestamp' class="row-timestamp"
-             v-bind:class="{ 'activeTimestamp': hasTimestamp(track.timestamp) && isFromPlayer}"
-             v-bind:title="getTitleAttribute(track.timestamp)"
-             v-on:click="callContentToPlayTrack(track.timestamp, isFromPlayer)">
-          {{track.time}}
-        </div>
-        <div class="row-artist">{{track.artistName}}</div>
-        <div class="row-track">
-          {{track.songName}}
-        </div>
-      </div>
-    </div>
+  <div v-else class="mwt-tracklist-container">
+    <table class="tracklist-table">
+      <thead>
+      <tr class="mwt-header">
+        <th scope="col" class="header-number">#</th>
+        <th scope="col" class="header-timestamp">Time</th>
+        <th scope="col" class="header-artist">Artist</th>
+        <th scope="col" class="header-track">Song</th>
+        <th class="header-copy">
+          <a class="copy-button" v-on:click="copyToClipoard(tracklist)">
+            <svg height="10pt" viewBox="-40 0 512 512" width="10pt" xmlns="http://www.w3.org/2000/svg">
+              <path d="m271 512h-191c-44.113281 0-80-35.886719-80-80v-271c0-44.113281 35.886719-80 80-80h191c44.113281 0 80 35.886719 80 80v271c0 44.113281-35.886719 80-80 80zm-191-391c-22.054688 0-40 17.945312-40 40v271c0 22.054688 17.945312 40 40 40h191c22.054688 0 40-17.945312 40-40v-271c0-22.054688-17.945312-40-40-40zm351 261v-302c0-44.113281-35.886719-80-80-80h-222c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h222c22.054688 0 40 17.945312 40 40v302c0 11.046875 8.953125 20 20 20s20-8.953125 20-20zm0 0"/>
+            </svg>
+          </a>
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="track in tracklist" >
+        <td id='trackNumber'
+            v-bind:class="{ 'pointer': hasTimestamp(track.timestamp) }"
+            v-bind:title="getTitleAttribute(track.timestamp)"
+            v-on:click="callContentToPlayTrack(track.timestamp)">
+          {{ track.trackNumber }}
+        </td>
+        <td id='timestamp'
+            v-bind:class="{ 'activeTimestamp': hasTimestamp(track.timestamp) && isFromPlayer }"
+            v-bind:title="getTitleAttribute(track.timestamp)"
+            v-on:click="callContentToPlayTrack(track.timestamp, isFromPlayer)">
+          {{ track.time }}
+        </td>
+        <td >{{ track.artistName }}</td>
+        <td colspan="2">{{ track.songName }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  props:[
+  props: [
     'tracklist', 'isFromPlayer', 'callContentToPlayTrack'
   ],
   methods: {
@@ -60,7 +70,7 @@ function getTitleAttribute (timestamp) {
 
 <style lang="css" scoped>
 p {
-  font-size: 20px;
+  font-size: 1.25rem;
 }
 
 * {
@@ -72,24 +82,17 @@ p {
   padding: 0px 10px 0px 10px;
 }
 
+.tracklist-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
 .mwt-header {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   line-height: 30px;
   border-top: 2px solid rgb(235, 240, 242);
   border-bottom: 2px solid rgb(235, 240, 242);
   color: rgb(105, 127, 149);
   font-weight: 600;
-}
-
-.table-row {
-  display: flex;
-  flex-direction: row;
-  line-height: 30px;
-  border-bottom: 1px solid rgb(235, 240, 242);
-  color: rgb(105, 127, 149);
-  font-weight: 400;
 }
 
 .header-number {
@@ -110,32 +113,9 @@ p {
 
 .header-copy {
   width: 5%;
-  padding-left: 12px;
   padding-bottom: 0px;
   padding-top: 7px;
-  color: rgb(30, 35, 55)
-}
-
-.row-number {
-  width: 5%;
-}
-
-.row-timestamp {
-  width: 15%;
-}
-
-.row-track {
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  width: 37.5%;
-}
-
-.row-artist {
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  width: 37.5%;
+  color: rgb(30, 35, 55);
 }
 
 .copy-button {
@@ -151,14 +131,9 @@ p {
   padding: 9px 16px;
   text-align: center;
   touch-action: manipulation;
-  -webkit-user-select: none;
+  user-select: none;
   vertical-align: middle;
   background-color: #fff;
-}
-
-.activeTimestamp {
-  cursor: pointer;
-  color: #4fa6d3
 }
 
 .copy-button:hover {
@@ -167,5 +142,21 @@ p {
 
 .copy-button:active {
   background-color: rgba(79, 166, 211, 0.8);
+}
+
+tbody > tr > td {
+  color: rgb(105, 127, 149);
+  font-weight: 400;
+  line-height: 1.875rem;
+  border-bottom: 1px solid rgb(235, 240, 242);
+}
+
+thead > tr > th {
+  text-align: left;
+}
+
+.activeTimestamp {
+  cursor: pointer;
+  color: #4fa6d3;
 }
 </style>
