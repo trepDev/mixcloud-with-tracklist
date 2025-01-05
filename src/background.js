f65bea70878947b9b80a849acd5e208f791dc152
trepDev <trepdev@fastmail.com>
2025-01-05 20:18:41 +0100

    Remove mix presence check in storeCloudcast method (redundant as the verification is already performed earlier)

--- src/background.js
+++ src/background.js
@@ -34,12 +34,12 @@ async function graphQLListener (spiedRequest) {
     if (payload.id !== 'MwT' && payload.query.includes('TracklistAudioPageQuery') &&
       !await store.getMixByPath('/' + payload.variables.lookup.username + '/' + payload.variables.lookup.slug + '/')) {
       chrome.tabs.query({ url: '*://*.mixcloud.com/*' }, (tabs) => {
-        if (tabs[0]) requestCloudcast(tabs[0], payload.variables)
+        if (tabs[0]) callContentForTracklistAudioPageQuery(tabs[0], payload.variables)
       })
-      // Not my own request  & Request for tracklist (with timestamp) & tracklist not already in store >> call content script for request cloudcast
+      // Not my own request  & Request for tracklist (with timestamp) & tracklist not already in store >> call content script to request cloudcast
     } else if (payload.id !== 'MwT' && payload.query.includes('PlayerControlsQuery') && !await store.getMixPathFromId(payload.variables.cloudcastId)) {
       chrome.tabs.query({ url: '*://*.mixcloud.com/*' }, (tabs) => {
-        if (tabs[0]) requestPlayerControlsQuery(tabs[0], payload.variables, payload.query)
+        if (tabs[0]) callContentForPlayerControlsQuery(tabs[0], payload.variables, payload.query)
       })
     }
   }
@@ -51,7 +51,7 @@ async function graphQLListener (spiedRequest) {
  * @param tabs
  * @param requestVariables
  */
-function requestCloudcast (tab, requestVariables) {
+function callContentForTracklistAudioPageQuery (tab, requestVariables) {
   chrome.tabs.sendMessage(
     tab.id,
     {
@@ -89,7 +89,7 @@ function requestCloudcast (tab, requestVariables) {
   )
 }
 
-function requestPlayerControlsQuery (tab, requestVariables, query) {
+function callContentForPlayerControlsQuery (tab, requestVariables, query) {
   chrome.tabs.sendMessage(tab.id,
     {
       action: 'requestTracklist',
@@ -116,14 +116,11 @@ function hasDataForPathInMixcloudResponse (response) {
 
 async function storeCloudcast (cloudcast, usernameAndSlug) {
   const mix = mixMapper.cloudcastToMix(cloudcast, usernameAndSlug)
-
-  if (!await store.getMixPathFromId(mix.id)) {
-    try {
-      await store.saveMix(mix)
-      await store.saveIdToPath(mix.id, mix.path)
-      console.log('saveMix ' + mix.path)
-    } catch (e) {
-      console.error(`Error on save for mix ${mix.path}`, e)
-    }
+  try {
+    await store.saveMix(mix)
+    await store.saveIdToPath(mix.id, mix.path)
+    console.log('saveMix ' + mix.path)
+  } catch (e) {
+    console.error(`Error on save for mix ${mix.path}`, e)
   }
 }
